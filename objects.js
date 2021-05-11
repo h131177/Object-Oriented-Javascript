2e994a275ec007e2350d206d128e096eb60fa6b1
h131177 <54102543+h131177@users.noreply.github.com>
2021-05-11 22:26:08 +0200

    Class Constructors

--- objects.js
+++ objects.js
@@ -1,8 +1,15 @@
 class User {
-
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
 }
 
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
 
+console.log(userOne);
+console.log(userTwo);
 
 // the 'new' kwyword
 // - creates a new empty object {}
