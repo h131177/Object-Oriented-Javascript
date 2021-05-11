12435b3f6f061cd46a3e660ac3cd809d36da2b65
h131177 <54102543+h131177@users.noreply.github.com>
2021-05-11 15:17:10 +0200

    Classes

--- objects.js
+++ objects.js
@@ -1,28 +1,3 @@
-var userOne = {
-    email: 'ryu@ninjas.com',
-    name: 'Ryu',
-    login() {
-        console.log(this.email, 'has logged in');
-    },
-    logout() {
-        console.log(this.email, 'has logged out');
-    }
-};
-
-// change the name property
-userOne.name = 'Yoshi';
-console.log(userOne.name);
-console.log(userOne['name']);
-userOne['name'] = 'mario';
-console.log(userOne.name);
-
-//dynamic property (can be chosen by user)
-var prop = 'name';
-console.log(userOne[prop]);
-prop = 'email';
-console.log(userOne[prop]);
-
-//add a new property (not the best way)
-//age should be in the object literal definition
-userOne.age = 25;
-console.log(userOne.age);
+class User {
+    
+}
