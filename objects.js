dea4db6348b7c552cfa5bcb433e268a39b153753
h131177 <54102543+h131177@users.noreply.github.com>
2021-05-11 14:06:42 +0200

    Object Literals

--- objects.js
+++ objects.js
@@ -1,32 +1,12 @@
-//Startcode or "spagetthi code"
+var userOne = {
+    email: 'ryu@ninjas.com',
+    name: 'Ryu',
+    login() {
+        console.log(this.email, 'has logged in');
+    },
+    logout() {
+        console.log(this.email, 'has logged out');
+    }
+};
 
-// create user one
-var userOneEmail = 'ryu@ninjas.com';
-var userOneName = 'Ryu';
-var userOneFriends = ['Yoshi'];
-
-// create user two
-var userTwoEmail = 'yoshi@mariocorp.com';
-var userTwoName = 'Yoshi';
-var userTwoFriends = ['Ryu', 'Mario'];
-
-// create user three
-var userThreeEmail = 'mario@mariocorp.com';
-var userThreeName = 'Mario';
-var userThreeFriends = ['Yoshi'];
-
-function login(email) {
-    console.log(email, 'is now online');
-}
-
-function logout(email) {
-    console.log(email, 'has logged out');
-}
-
-function logFriends(friends) {
-    friends.forEach(friend => {
-        console.log(friend);
-    });
-}
-
-login(userOneEmail);
+console.log(userOne.name);
