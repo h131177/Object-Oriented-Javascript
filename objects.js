015540cb30f8a199781d157cd5a79d60e4795d04
h131177 <54102543+h131177@users.noreply.github.com>
2021-05-11 14:30:28 +0200

    Updating properties

--- objects.js
+++ objects.js
@@ -9,4 +9,20 @@ var userOne = {
     }
 };
 
+// change the name property
+userOne.name = 'Yoshi';
 console.log(userOne.name);
+console.log(userOne['name']);
+userOne['name'] = 'mario';
+console.log(userOne.name);
+
+//dynamic property (can be chosen by user)
+var prop = 'name';
+console.log(userOne[prop]);
+prop = 'email';
+console.log(userOne[prop]);
+
+//add a new property (not the best way)
+//age should be in the object literal definition
+userOne.age = 25;
+console.log(userOne.age);
